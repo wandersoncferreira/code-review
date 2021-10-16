e91ba2b9bb68bc4589e8f5c94de9ec21b11cff2c
Wanderson Ferreira <wand@hey.com>
2021-10-16 14:53:24 -0300

    small diff functions

--- code-review-diff.el
+++ code-review-diff.el
@@ -20,6 +20,28 @@
 ;;; Code:
 
 
+(defun code-review-diff-hunk? (line)
+  "Current LINE in the buffer."
+  (string-prefix-p "@@" line))
+
+(defun code-review-diff-added? (line)
+  "Current LINE in the buffer."
+  (string-prefix-p "+ " line))
+
+(defun code-review-diff-removed? (line)
+  "Current LINE in the buffer."
+  (string-prefix-p "- " line))
+
+(defun code-review-diff-header? (line)
+  "Current LINE in the buffer."
+  (or (string-prefix-p "diff --git" line)
+      (string-prefix-p "index" line)))
+
+(defun code-review-diff-file-header? (line)
+  "Current LINE in the buffer."
+  (or (string-prefix-p "--- " line)
+      (string-prefix-p "+++ " line)))
+
 
 (provide 'code-review-diff)
 ;;; code-review-diff.el ends here
