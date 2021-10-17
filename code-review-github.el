266117b8549cd9e19a9877cb32e77a05f8b777f5
Wanderson Ferreira <wand@hey.com>
2021-10-17 13:12:48 -0300

    github functions to send review and replies

--- code-review-github.el
+++ code-review-github.el
@@ -122,6 +122,48 @@ returns a deferred object"
                       d))
     d))
 
+(defun code-review-github-post-review (pr-alist review callback)
+  "Submit a code review.
+PR-ALIST is an alist representing a PR
+REVIEW is the review alist
+CALLBACK will be called back when done"
+  (let-alist pr-alist
+    (ghub-post (format "/repos/%s/%s/pulls/%s/reviews" .owner .repo .num)
+               nil
+               :auth 'code-review
+               :payload review
+               :host code-review-github-host
+               :errorback #'code-review-github-errback
+               :callback callback)))
+
+(defun code-review-github-post-replies (pr-alist replies callback)
+  "Submit REPLIES to review comments inline given PR-ALIST and a CALLBACK fn."
+  (deferred:$
+    (deferred:parallel
+      (-map
+       (lambda (reply)
+         (lambda ()
+           (let-alist pr-alist
+             (let* ((database-id (a-get reply 'database-id))
+                    (body (a-get reply 'body)))
+               (ghub-post (format "/repos/%s/%s/pulls/%s/comments/%s/replies"
+                                  .owner .repo .num database-id)
+                          nil
+                          :payload body
+                          :headers code-review-github-diffheader
+                          :auth 'code-review
+                          :host code-review-github-host
+                          :callback (lambda (&rest _))
+                          :errorback #'code-review-github-errback)))))
+       replies))
+
+    (deferred:nextc it
+      (lambda (x)
+        (funcall callback)))
+
+    (deferred:error it
+      (lambda (err)
+        (message "Got an error from the Github Reply API %S!" err)))))
 
 (provide 'code-review-github)
 ;;; code-review-github.el ends here
