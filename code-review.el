ccf8dc4793d62da2166afa5010aa063fc2b6390b
Wanderson Ferreira <wand@hey.com>
2021-10-25 00:04:07 -0300

    more deps

--- code-review.el
+++ code-review.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: git, tools, vc
 ;; Homepage: https://github.com/wandersoncferreira/code-review
-;; Package-Requires: ((emacs "25.1") (closql "1.2.0") (magit "3.0.0"))
+;; Package-Requires: ((emacs "25.1") (closql "1.2.0") (magit "3.0.0") (a "1.0.0") (ghub "3.5.1"))
 
 ;; This file is not part of GNU Emacs
 
