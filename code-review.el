f9786210b9d4642db3a4240621b4e89d5e796a18
Wanderson Ferreira <wand@hey.com>
2021-12-04 09:00:03 -0300

    release v0.0.3

--- code-review.el
+++ code-review.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Wanderson Ferreira <wand@hey.com>
 ;; Created: October 14, 2021
 ;; Modified: October 14, 2021
-;; Version: 0.0.2
+;; Version: 0.0.3
 ;; Keywords: git, tools, vc
 ;; Homepage: https://github.com/wandersoncferreira/code-review
 ;; Package-Requires: ((emacs "25.1") (closql "1.2.0") (magit "3.0.0") (a "1.0.0") (ghub "3.5.1") (uuidgen "1.2") (deferred "0.5.1") (markdown-mode "2.4") (forge "0.3.0") (emojify "1.2"))
